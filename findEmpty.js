e52acc9022bcb27707e723fa01cf038db6712bb1
Anna <kalinina_anna_spb@mail.ru>
2022-08-12 15:05:14 +0300

    cicle 2

--- findEmpty.js
+++ findEmpty.js
@@ -1,5 +1,7 @@
 const findEmpty = (sudoku) => {
-for (let r = 0; r < ResizeObserverSize; r++) {
-    
+for (let r = 0; r < size; r++) {
+    for (let c = 0; c < size; c ++) {
+        
+    }
 }
 };
