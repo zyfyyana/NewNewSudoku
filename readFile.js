ea86d3e1917a675cd7202c55cbb693f82136508d
nikolayvadimovich <a111@MacBook-Pro-111.local>
2022-08-12 16:07:52 +0300

    Сделана функция

--- readFile.js
+++ readFile.js
@@ -1,12 +1,18 @@
 const fs = require('fs')
-// function sudokuChoose(num){
+ function sudokuChoose(num){
     let sudokuPuzzles= fs.readFileSync('./puzzles.txt', 'utf-8').split('\n');
+    const sudokus = sudokuPuzzles.map(el => el.split('')).slice(0, sudokuPuzzles.length - 1);
+    let arra2 =[]
+    let arra =[]
+    for (let i1 = 0; i1 < sudokus.length; i1++){
+        arra =[]
+        for (let i2 = 0; i2 < sudokus[i1].length;i2=i2+9){
+            
+            arra.push(sudokus[i1].slice(i2,i2+9))
+        }
+        arra2.push(arra)
+    }
+return arra2[num-1]
+ }
 
-//     let sudokuArr = sudokuPuzzles.map(el=>[el].replace('/.{1-9}/g', '')).slice(0,sudokuPuzzles.length-1)
-console.log(sudokuPuzzles)
-
-// for(let i1 = 0; i1 < sudokuArr.length; i1++){
-//     for(let i2; i2 < sudokuArr[i1].length;i2++){
-
-//     }
-// }
+ console.log(sudokuChoose(15))
