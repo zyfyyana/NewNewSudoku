7962880da601bf64051f2dab116b7ba660822d15
Anna <kalinina_anna_spb@mail.ru>
2022-08-12 15:07:35 +0300

    func done

--- findEmpty.js
+++ findEmpty.js
@@ -1,7 +1,11 @@
 const findEmpty = (sudoku) => {
 for (let r = 0; r < size; r++) {
-    for (let c = 0; c < size; c ++) {
-        
+    for (let c = 0; c < size; c++) {
+        if (sudoku[r][c] === '.') {
+            return [r, c];
+        }
     }
 }
+    return null;
 };
+
