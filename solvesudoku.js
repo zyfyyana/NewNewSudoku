16a37e4d628c4cc5c442d821c34780e3514246ce
Kleonisy <neron0195@mail.ru>
2022-08-12 15:24:48 +0300

    solve and refactoring imports

--- solvesudoku.js
+++ solvesudoku.js
@@ -1,3 +1,7 @@
+const findEmptyAnya = require('findEmpty');
+const validateYana = require('validate');
+const inputKolya = require('readFile');
+
 
 const solveSudoku = (sudoku) => {
     //размерность таблицы и размерность секторов
@@ -6,13 +10,44 @@ const solveSudoku = (sudoku) => {
 
 
     // поиск незаполненных ячеек - Анина функция
-    const findEmpty 
+    const findEmpty = findEmptyAnya;
 
     // проверка перебираемых в цикле solving значений на валидность - Яна
-    const validate
+    const validate = validateYana;
 
     // решение
+    const solving = () => {
+        const currentPos = findEmpty(sudoku);
 
-    
+        if (currentPos === null) {
+            return true;
+        }
+
+        // перебор всех чисел для пустых ячеек
+        for (let i = 1; i < size + 1; i += 1) {
+            const currentNumber = i.toString();
+            const isValid = validate(currentNumber, currentPos, sudoku);
+
+            // финал очка
+            if (isValid) {
+                const [x, y] = currentPos;
+                sudoku[x][y] = currentNumber;
+
+                if (solving()) {
+                    return true;
+                }
+
+                sudoku[x][y] = '.';
+            }
+        }
+
+        return false;
+    }
+
+
+    solving();
+
     return sudoku;
 };
+
+// console.log(sudoku(inputKolya(1)));
