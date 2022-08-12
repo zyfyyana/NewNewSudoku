4c1faa7b6fe216b7ee946cd67e400daefea499bb
Kleonisy <neron0195@mail.ru>
2022-08-12 15:05:33 +0300

    file template

--- solvesudoku.js
+++ solvesudoku.js
@@ -1,4 +1,18 @@
 
 const solveSudoku = (sudoku) => {
+    //размерность таблицы и размерность секторов
+    const size = 9;
+    const sectorSize = 3;
+
+
+    // поиск незаполненных ячеек - Анина функция
+    const findEmpty 
+
+    // проверка перебираемых в цикле solving значений на валидность - Яна
+    const validate
+
+    // решение
+
     
+    return sudoku;
 };
